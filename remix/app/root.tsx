af54b0f0c4c5c19290673643344de01c62bcdcbf
Wes Feller <wes@commandbar.com>
2022-05-16 23:50:27 -0700

    feat(commandbar): 7GUIs CRUD for Remix

--- remix/app/root.tsx
+++ remix/app/root.tsx
@@ -3,12 +3,16 @@ import {
   Links,
   LiveReload,
   Meta,
+  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
   useNavigate,
 } from "@remix-run/react";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import stylesheetUrl from "./global.css";
+
+export const links = () => [{ rel: "stylesheet", href: stylesheetUrl }];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -16,24 +20,173 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
+export type Person = { first: string; last: string };
+export type ContextType = {
+  people: Person[];
+  create: (person: Person) => void;
+  update: ({ selected, first, last }: { selected: Person } & Person) => void;
+  remove: ({ selected }: { selected: Person }) => void;
+};
+
 export default function App() {
   const navigate = useNavigate();
 
+  const [people, setPeople] = useState([
+    { first: "Hans", last: "Emil" },
+    { first: "Max", last: "Mustermann" },
+    { first: "Roman", last: "Tisch" },
+  ]);
+
+  const selectedPersonIndex = useCallback(
+    (selected: Person) =>
+      people.findIndex(
+        (person) =>
+          person.first === selected.first && person.last === selected.last
+      ),
+    [people]
+  );
+
+  const create = useCallback(
+    ({ first: _first, last: _last }: Person) =>
+      setPeople([...people, { first: _first, last: _last }]),
+    [people]
+  );
+  const update = useCallback(
+    ({
+      selected: _selected,
+      first: _first,
+      last: _last,
+    }: { selected: Person } & Person) =>
+      setPeople(
+        people.map((person, i) =>
+          i === selectedPersonIndex(_selected)
+            ? { first: _first, last: _last }
+            : person
+        )
+      ),
+    [people, selectedPersonIndex]
+  );
+  const remove = useCallback(
+    ({ selected: _selected }: { selected: Person }) =>
+      setPeople(() => {
+        const index = selectedPersonIndex(_selected);
+        return [...people.slice(0, index), ...people.slice(index + 1)];
+      }),
+    [people, selectedPersonIndex]
+  );
+
+  const context: ContextType = { people, create, update, remove };
+  const outlet = <Outlet context={context} />;
+
   useEffect(() => {
-    window.CommandBar.boot("me").then(() => {
-      window.CommandBar.addCommand({
-        name: "Home",
-        text: "Home",
-        category: "Navigation",
-        template: { type: "link", value: "/", operation: "router" },
-      });
-      window.CommandBar.addCommand({
-        name: "Fop",
-        text: "Fop",
-        category: "Navigation",
-        template: { type: "link", value: "/fop", operation: "router" },
-      });
+    void window.CommandBar.boot("me");
+    return window.CommandBar.shutdown;
+  }, []);
+
+  useEffect(() => {
+    window.CommandBar.addContext(
+      "people",
+      () =>
+        people.map(({ first, last }, id) => ({
+          id,
+          label: `${first} ${last}`,
+          first,
+          last,
+        })),
+      {
+        quickFindOptions: { quickFind: true },
+      }
+    );
+  }, [people]);
+
+  useEffect(() => {
+    window.CommandBar.addCallback("create", create);
+  }, [create]);
+
+  useEffect(() => {
+    window.CommandBar.addCallback("update", update);
+  }, [update]);
+
+  useEffect(() => {
+    window.CommandBar.addCallback("remove", remove);
+  }, [remove]);
+
+  useEffect(() => {
+    void window.CommandBar.addCommand({
+      name: "Home",
+      text: "Home",
+      category: "Navigation",
+      template: { type: "link", value: "/", operation: "router" },
     });
+    void window.CommandBar.addCommand({
+      name: "Foo",
+      text: "Foo",
+      category: "Navigation",
+      template: { type: "link", value: "/foo", operation: "router" },
+    });
+
+    void window.CommandBar.addCommand({
+      name: "create",
+      text: "Add a person",
+      category: "CRUD",
+      icon: "https://openmoji.org/data/color/svg/E268.svg",
+      template: { type: "callback", value: "create" },
+      arguments: {
+        first: { order_key: 0, value: "text", type: "provided" },
+        last: { order_key: 1, value: "text", type: "provided" },
+      },
+    });
+    void window.CommandBar.addCommand({
+      name: "update",
+      text: "Update a person",
+      category: "CRUD",
+      icon: "https://openmoji.org/data/color/svg/E25D.svg",
+      template: { type: "callback", value: "update" },
+      arguments: {
+        selected: {
+          order_key: 0,
+          value: "people",
+          type: "context",
+          label: "Person to update",
+        },
+        first: {
+          order_key: 1,
+          value: "text",
+          type: "provided",
+          label: "New first name",
+        },
+        last: {
+          order_key: 2,
+          value: "text",
+          type: "provided",
+          label: "New last name",
+        },
+      },
+    });
+
+    void window.CommandBar.addCommand({
+      name: "remove",
+      text: "Remove a person",
+      category: "CRUD",
+      icon: "https://openmoji.org/data/color/svg/E262.svg",
+      template: { type: "callback", value: "remove" },
+      arguments: {
+        selected: {
+          order_key: 0,
+          value: "people",
+          type: "context",
+          label: "Person to update",
+        },
+      },
+    });
+
+    return () => {
+      window.CommandBar.removeCommand("home");
+      window.CommandBar.removeCommand("foo");
+      window.CommandBar.removeCommand("create");
+      window.CommandBar.removeCommand("update");
+      window.CommandBar.removeCommand("remove");
+    };
   }, []);
 
   useEffect(() => {
@@ -47,7 +200,11 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/foo">Foo</NavLink>
+        </nav>
+        {outlet}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- remix/app/routes/index.tsx
+++ remix/app/routes/index.tsx
@@ -1,7 +1,123 @@
+import { useCallback, useEffect, useState } from "react";
+import { useOutletContext } from "@remix-run/react";
+import type { ContextType, Person } from "~/root";
+
 export default function Index() {
+  const context = useOutletContext<ContextType>();
+  const [prefix, setPrefix] = useState("");
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
+  const [index, setIndex] = useState(0);
+  const [filteredPeople, setFilteredPeople] = useState(
+    prefix
+      ? context?.people.filter((person) => {
+          const name = `${person.last}, ${person.first}`;
+          return name.toLowerCase().startsWith(prefix.toLowerCase());
+        })
+      : context?.people
+  );
+  const [selected, setSelected] = useState<Person>();
+
+  useEffect(() => {
+    setFilteredPeople(
+      prefix
+        ? context?.people.filter((person) => {
+            const name = `${person.last}, ${person.first}`;
+            return name.toLowerCase().startsWith(prefix.toLowerCase());
+          })
+        : context?.people
+    );
+  }, [context?.people, prefix]);
+
+  useEffect(() => {
+    if (filteredPeople) {
+      setSelected(filteredPeople[index]);
+    }
+  }, [filteredPeople, index]);
+
+  const reset_inputs = useCallback((person: Person) => {
+    setFirst(person ? person.first : "");
+    setLast(person ? person.last : "");
+  }, []);
+
+  useEffect(() => {
+    if (selected) {
+      reset_inputs(selected);
+    }
+  }, [reset_inputs, selected]);
+
   return (
     <main>
-      <h1>Home</h1>
+      <input
+        placeholder="filter prefix"
+        onChange={(e) => {
+          setPrefix(e.target.value);
+        }}
+        value={prefix}
+      />
+
+      <select
+        onChange={(e) => {
+          setIndex(Number(e.target.value));
+        }}
+        size={5}
+      >
+        {filteredPeople?.map((person, i) => (
+          <option key={i} value={i}>
+            {person.last}, {person.first}
+          </option>
+        ))}
+      </select>
+
+      <label>
+        <input
+          onChange={(e) => {
+            setFirst(e.target.value);
+          }}
+          value={first}
+          placeholder="first"
+        />
+      </label>
+      <label>
+        <input
+          onChange={(e) => {
+            setLast(e.target.value);
+          }}
+          value={last}
+          placeholder="last"
+        />
+      </label>
+
+      <div className="buttons">
+        <button
+          onClick={() => {
+            context?.create({ first, last });
+          }}
+          disabled={!first || !last}
+        >
+          create
+        </button>
+        <button
+          onClick={() => {
+            if (selected) {
+              context?.update({ selected, first, last });
+            }
+          }}
+          disabled={!first || !last || !selected}
+        >
+          update
+        </button>
+        <button
+          onClick={() => {
+            if (selected) {
+              context?.remove({ selected });
+            }
+          }}
+          disabled={!selected}
+        >
+          delete
+        </button>
+      </div>
     </main>
   );
 }
